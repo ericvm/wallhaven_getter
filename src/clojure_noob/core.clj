41ad5b672d52c6f94e4a35543bf6fb271eed28cd
Eric Velten de Melo <eric.velten@eldorado.org.br>
2017-01-10 12:11:35 -0200

    Filtered images by jpg extension

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -6,12 +6,17 @@
 
 (defn parse-html [content]
   "Gets list of image ids from wallhaven html body"
-  (map second (re-seq #"https://alpha.wallhaven.cc/wallpaper/(\d+)" content))
-)
+  (map second (re-seq #"https://alpha.wallhaven.cc/wallpaper/(\d+)" content)))
+
+(defn parse-format [content]
+  "Gets image extension from html body"
+  (second (re-find #"<img id=\"wallpaper\".*wallhaven-\d+\.([a-z]+)" content)))
+
+(defn make-img-page [id]
+  (str "https://alpha.wallhaven.cc/wallpaper/" id))
 
 (defn make-img-url [id]
-  (str "https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-" id ".jpg")
-)
+  (str "https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-" id ".jpg"))
 
 (defn make-output-path [root id]
   (str root "/wall_" id ".jpg"))
@@ -31,6 +36,13 @@
         http/string
         parse-html)))
 
+(defn get-img-extension [client id]
+  (let [response (http/GET client (make-img-page id))]
+    (-> response
+        http/await
+        http/string
+        parse-format)))
+
 (defn save-img [binaryimage file]
   (with-open [out (io/output-stream file)]
     (io/copy binaryimage out)))
@@ -38,8 +50,9 @@
 (defn -main
   [url & args]
   (with-open [client (http/create-client)]
-    (def id-list (take 9 (set (get-image-ids client url))))
-    (def images (doall (map (partial get-img client) id-list))))
+    (def id-list (set (get-image-ids client url)))
+    (def jpg-ids (filter (fn [x] (= (get-img-extension client x) "jpg")) id-list))
+    (def images (doall (map (partial get-img client) (take 9 jpg-ids)))))
   (doall (for [[image id] (map vector images (range 1 10))]
      (save-img image (make-output-path "/home/eric/.wall" id))))
 )
